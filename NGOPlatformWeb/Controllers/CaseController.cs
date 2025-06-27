275719a8e1b4f51e30ea6700d8aac45f3d4d6202
Yuqi26 <lanlin19991232@gmail.com>
2025-06-27 10:42:22 +0800

    initial commit

---
+++ NGOPlatformWeb/Controllers/CaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NGOPlatformWeb.Controllers
+{
+    public class CaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
